62c445ecb039a7042f2482c728597f47a15b43d7
surendhar R <surendhar2339@gmail.com>
2023-06-21 10:00:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require("cors")
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config();
-//testing aad ad
+// new test update 
 app.use(express.json()); // Parse JSON request bodies
 app.use(cors()); // cross orign resorce sharing
 
